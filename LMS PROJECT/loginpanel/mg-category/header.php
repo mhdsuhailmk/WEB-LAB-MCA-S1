27be8df35850f8ac83a80fb53789e8dd58ee81f4
Mhd Suhail Mk <mksuhail787@gmail.com>
2022-03-03 15:05:04 +0530

    Add files via upload

---
+++ LMS PROJECT/loginpanel/mg-category/header.php
@@ -0,0 +1,34 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Library Management System</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+    <!-- Normalize CSS -->
+    <link rel="stylesheet" href="../css/normalize.css">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="../css/main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="../css/all.min.css">
+    <!-- Flaticon CSS -->
+    <link rel="stylesheet" href="../fonts/flaticon.css">
+
+    <link rel="stylesheet" href="../css/fullcalendar.min.css">
+
+    <link rel="stylesheet" href="../css/select2.min.css">
+    <link rel="stylesheet" href="../css/datepicker.min.css">
+    
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="../css/animate.min.css">
+
+    <link rel="stylesheet" href="../css/jquery.dataTables.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- Modernize js -->
+    <script src="../js/modernizr-3.6.0.min.js"></script>
+</head>
---
+++ LMS PROJECT/loginpanel/mg-category/scripts.php
@@ -0,0 +1,27 @@
+<!-- jquery-->
+<script src="../js/jquery-3.3.1.min.js"></script>
+<!-- Plugins js -->
+<script src="../js/plugins.js"></script>
+<!-- Popper js -->
+<script src="../js/popper.min.js"></script>
+<!-- Bootstrap js -->
+<script src="../js/bootstrap.min.js"></script>
+
+<script src="../js/select2.min.js"></script>
+
+<script src="../js/datepicker.min.js"></script>
+
+
+<!-- Counterup Js -->
+<script src="../js/jquery.counterup.min.js"></script>
+
+ 
+<!-- Scroll Up Js -->
+<script src="../js/jquery.scrollUp.min.js"></script>
+<!-- Full Calender Js -->
+<script src="../js/fullcalendar.min.js"></script>
+
+<script src="../js/jquery.dataTables.min.js"></script>
+
+<!-- Custom Js -->
+<script src="../js/main.js"></script>
